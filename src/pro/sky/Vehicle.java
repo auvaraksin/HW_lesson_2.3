185d6ffafc24e12d2658fc5863c7f82a6344860f
auvararaksin <auvaraksin@mail.ru>
2022-03-18 17:08:37 +0200

    HW_lesson_2.3 version 1.0 gitignore list has been updated

---
+++ src/pro/sky/Vehicle.java
@@ -0,0 +1,19 @@
+package pro.sky;
+
+public abstract class Vehicle {
+    private final String modelName;
+    private final int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+}
---
+++ src/pro/sky/VehicleServiceInterface.java
@@ -0,0 +1,7 @@
+package pro.sky;
+
+public interface VehicleServiceInterface {
+    public void updateTyre();
+    public void checkEngine();
+    public void checkTrailer();
+}
